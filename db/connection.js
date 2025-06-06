c4384fa77945f59b96bd26a764214188ee741def
IvanW5X <ivanwong169@gmail.com>
2025-06-05 23:44:48 -0700

    Added outlines for dashboards

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,19 @@
 // Module Imports & Config
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 // Database Connection Setup
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 // Connect to Database
-connection.connect(err => {
-  if (err) throw err;
-  console.log('Connected to the database.');
+connection.connect((err) => {
+  if (err) console.log(err);
+  else console.log("Connected to the database.");
 });
 
 // Export Connection
